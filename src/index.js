a43cb9855c0da64137ecf48d58074b5f7a6368a4
Ravindudilusha <IT21060380@my.sliit.lk>
2024-06-23 07:36:48 +0530

    google analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-KM1Q664CLY");
+ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
